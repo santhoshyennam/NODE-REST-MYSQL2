c8e8f8628789a5c2f1779cee66c4c763c1dfbd1b
santhoshyennam <sandysanthosh045@gmail.com>
2024-04-24 18:15:12 -0400

    used env file

--- connection.js
+++ connection.js
@@ -1,9 +1,10 @@
+require('dotenv').config();
 const mysql = require("mysql2");
 
 const connection = mysql.createConnection({
     host: "localhost",
-    user: "root",
-    password: "",
+    user: process.env.db_username,
+    password: process.env.db_password,
     database: "metiz"
 });
 
